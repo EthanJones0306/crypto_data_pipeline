8fd157dbdcc341247d8935f4ad8bccd56c6702f5
EthanJones0306 <jnseth005@myuct.ac.za>
2026-04-24 11:06:36 +0200

    Create cursor object for db connection

--- fetch_crypto.py
+++ fetch_crypto.py
@@ -4,3 +4,4 @@ from datetime import datetime
 
 # Set up the database 
 conn = sqlite3.connect('crypto.db') # Connect project database
+cursor = conn.cursor() # Create a table to store cryptocurrency data
